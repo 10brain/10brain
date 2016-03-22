1b38a20afadde3155ef7ee85e9d610920033fc58
afurukawa-10brain <a.furukawa@10baton.com>
2016-03-23 03:16:00 +0900

    login.htmlから飛んだ場合とadmin/header.htmlから飛んだ場合とでは適用されるcssが違ってくる

--- admin/top/top.php
+++ admin/top/top.php
@@ -10,18 +10,20 @@ $Key2 ="";
 
 ///^[a-zA-Z0-9!$&*.=^`|~#%'+\/?_{}-]+@([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,6}$/
 //IDとパスワードチェック
-if (!ckStr($_POST["KEYWORD1"],30,1) or !preg_match("^[a-zA-Z0-9]+$",$_POST["KEYWORD1"])){
+if (!ckStr($_POST["KEYWORD1"],30,1) or ereg("^[a-zA-Z0-9]+$",$_POST["KEYWORD1"])){
     $result = 1;
 }elseif (!ckStr($_POST["KEYWORD2"],30,1)){
     $result = 1;
 }else{
     $ActType = $_POST["ActionType"];
     $Key1 = $_POST["KEYWORD1"];  //ID
-    $Key2 = $_POST["KEYWORD2"];  //パスワード
+    $Key2 = $_POST["KEYWORD2"];
 
     //DB問い合わせ
     $obj=new UserModel();
     $result = $obj->GETLogin($ActType, $Key1, $Key2, $dspUserInfo);
+    $Key3 = $dspUserInfo[1];
+    $Key0 = $dspUserInfo[0];
 
 }
 
@@ -37,7 +39,7 @@ if ($result == 0){
 	$error = "ただいまサーバーが込み合っております。";
     }
 
-include("login.html");
+include("../../login/login.html");
 }
 
 
