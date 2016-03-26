89942bd8d311f8f92b3ef3dd97cfee2633b24c6a
伊藤麻衣 <m.ito@10baton.com>
2016-03-26 15:48:15 +0900

    詳細から貸出の三冊以上のときの処理

--- book/book_conf.php
+++ book/book_conf.php
@@ -31,33 +31,45 @@ if (!ckStr($_POST["KEYWORD1"],30,1) or ereg("^[a-zA-Z0-9]+$",$_POST["KEYWORD1"])
     echo $Key42;
 echo $Key43;
 
-    if (!preg_match("/^[0-9]+$/", $Key40)){
-        $bnum_error = "書籍番号が正しくありません。";
-        $result = 4;
-    }
-    if(is_null($Key43)){
-        $title_error = "書籍タイトルが正しくありません。";
-        $result = 4;
-    }
-    if(is_null($Key42)){
-        $day_error = "日付が正しくありません。";
-        $result = 4;
-    }
-    if($result==0){
-        $book='book_dec.php';
-        include 'book_confirm.html';
-    }else{
-        if ($_POST["ActionType"] != "TgRSPInf"){
-            $error = "";
-        }elseif ($result == 1){
-            $error = "入力内容に誤りがあります。再度入力してください。";
-        }else{
-            $error = "ただいまサーバーが込み合っております。";
-        }
-
-        include("../login/login.html");
-    }
 }
+
+    //DB問い合わせ
+    $obj=new BookModel();
+    //入力された情報の確認
+    $result = $obj->GETBorrowUList($ActType, $Key0, $dspBorrowUList);
+    
+    if(array_count_values(is_null($dspBorrowUList[$i][3])) <= 3){
+        include("book_not.html");
+    }else{
+        if (!preg_match("/^[0-9]+$/", $Key40)){
+            $bnum_error = "書籍番号が正しくありません。";
+            $result = 4;
+        }
+        if(is_null($Key43)){
+            $title_error = "書籍タイトルが正しくありません。";
+            $result = 4;
+        }
+        if(is_null($Key42)){
+            $day_error = "日付が正しくありません。";
+            $result = 4;
+        }
+    }
+    
+if($result==0){
+    $book='book_dec.php';
+    include 'book_confirm.html';
+}else{
+    if ($_POST["ActionType"] != "TgRSPInf"){
+        $error = "";
+    }elseif ($result == 1){
+        $error = "入力内容に誤りがあります。再度入力してください。";
+    }else{
+        $error = "ただいまサーバーが込み合っております。";
+    }
+
+    include("../login/login.html");
+}
+
 
 
 ?>
